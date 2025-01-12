21bec4e6eb352b799b78710bcf09b721541d4840
Daniel Rudrich <mail@danielrudrich.de>
2025-01-12 20:11:13 +0100

    adds tauri configuration for standalone application

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    output: "export",
 };
 
 export default nextConfig;
